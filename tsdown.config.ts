403aa6b6dddc53992a741b119ebf847b1cf51ea9
Jeff Wang <229298746@qq.com>
2026-01-05 14:37:03 +0800

    fix: remove peer vitepress

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: './src/index.ts',
+  external: ['vue', 'vitepress'],
   platform: 'neutral',
   fromVite: true,
   dts: { vue: true },
